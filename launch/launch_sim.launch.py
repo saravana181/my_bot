4cfa02a4282498c13da24745f31e10a5e084823a
sara <sara@Ubuntu-22.04.myguest.virtualbox.org>
2023-02-19 23:15:55 -0600

    ros2 control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -36,6 +36,19 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
 
 
 
@@ -44,4 +57,6 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])

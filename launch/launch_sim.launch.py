11fb03fe4212681ef8082a65b192f4aeb2183633
sara <sara@ubuntu.(none)>
2023-04-16 18:27:32 -0500

    Joystick update

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]),
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')

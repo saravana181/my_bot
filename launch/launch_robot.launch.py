ad16fb2311421cbd6b3b804d87d0d72fc7788d3c
sara <sara@ubuntu.(none)>
2023-04-16 20:17:56 -0500

    RPlidar timer

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -34,6 +34,8 @@ def generate_launch_description():
                 )]),
     )
 
+    delayed_rplidar = TimerAction(period=3.0, actions=[rplidar])
+
     joystick = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
@@ -110,7 +112,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        rplidar,
+        delayed_rplidar,
         joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
